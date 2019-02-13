9e37135c80b11943a5be4386db5a8c1692020103
Kevin Smith <keveightysev@gmail.com>
2019-02-13 14:31:35 -0600

    Added "Add Task" input form, built inputTask function for input field

--- src/App.js
+++ src/App.js
@@ -27,15 +27,29 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  inputTask = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>Todo List</h2>
-        {this.state.tasks.map(task => {
-          return (
-            <p>{task.task}</p>
-          )
-        })}
+        <div className="task-list">
+          <h2>Todo List</h2>
+          {this.state.tasks.map(task => {
+            return (
+              <p>{task.task} - {task.id}</p>
+            )
+          })}
+        </div>
+        <div className="add-task">
+          <input type="text" value={this.state.task} onChange={this.inputTask} name="task" placeholder="Add task"/>
+          <button>Add</button>
+          <button>Clear Completed</button>
+        </div>
       </div>
     );
   }
