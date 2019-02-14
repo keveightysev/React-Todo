685dd9a632c5e0860594280f7bd167226aaa9ce2
Kevin Smith <keveightysev@gmail.com>
2019-02-14 17:33:33 -0600

    Styled tasks

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './Todo.css';
 
 const Todo = (props) => {
+    const taskDate = props.task.id;
+    console.log(`${taskDate.toString()}`)
     return (
-        <p className={`${props.task.completed ? 'done' : null}`} onClick={() => props.markCompleted(props.task.id)}>{props.task.task} - {props.task.id}</p>
+        <div className={`task `} onClick={() => props.markCompleted(props.task.id)}>
+        <input type="checkbox" className="check" checked={props.task.completed ? 'done' : null} />
+        <p className={`${props.task.completed ? 'done' : null}`}>{props.task.task}</p>
+        </div>
     );
 }
 
