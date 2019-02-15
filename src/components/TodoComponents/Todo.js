b66dff283befd9aba91dd922dd8e99dd2921e235
Kevin Smith <keveightysev@gmail.com>
2019-02-15 02:40:48 -0600

    Updated some styling so that when rendered on iOS it looks like it should

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -4,7 +4,7 @@ import './Todo.css';
 const Todo = (props) => {
     return (
         <div className={`task`} onClick={() => props.markCompleted(props.task.id)}>
-        <input type="checkbox" className="check" checked={props.task.completed ? 'done' : null} readOnly />
+        <input type="checkbox" className="check" checked={props.task.completed ? true : false} />
         <p className={`task-text`}><span className={props.task.completed ? 'done' : null}>{props.task.task}</span></p>
         </div>
     );
